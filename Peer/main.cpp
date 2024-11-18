0b78be1448a5ef607ee0e69731dc908f173db74a
wpfsilvaa <wpf.silvaa@outlook.com>
2024-11-18 15:39:45 -0300

    Atualiza código do Peer para enviar as transações do usuario

--- Peer/main.cpp
+++ Peer/main.cpp
@@ -1,13 +1,21 @@
 #include "peer.hpp"
-
+#include "iostream"
 int main() {
     const std::string serverHost = "127.0.0.1";
+    std::string remetente, destinatario;
+    double qtd;
     const int serverPort = 8080;
 
     Peer peer(serverHost, serverPort);
 
     peer.connectToServer();
-    peer.sendTransaction("Alice", "Bob", 50.0);
+    std::cout << "Remetente:";
+    std::cin >> remetente;
+    std::cout << "Destinatario:";
+    std::cin >> destinatario;
+    std::cout << "Valor:";
+    std::cin >> qtd;
+    peer.sendTransaction(remetente, destinatario, qtd);
     peer.requestBlockchain();
 
     return 0;
